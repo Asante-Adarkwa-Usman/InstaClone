96b4d7c8485676bf891e86ec9a440a8c0ef78f9a
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2022-12-01 23:33:37 -0500

    Header for home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import NavSwitcher from './src/navigation';
+import {MD3LightTheme as DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
  
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'white',
+    secondary: 'black',
+  },
+};
+
 const App = () => {
   return (
+    <PaperProvider theme={theme} >
     <NavigationContainer>
         <NavSwitcher />
     </NavigationContainer>
+    </PaperProvider>
   );
 };
 
